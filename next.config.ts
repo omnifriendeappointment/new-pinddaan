b8768708017f6d753c49fb8b87d1ad818d91134f
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2025-08-18 23:06:48 +0530

    prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+const repo = '/new-pinddaan';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: isProd ? repo : '',
+  assetPrefix: isProd ? `${repo}/` : '',
 };
 
 export default nextConfig;
