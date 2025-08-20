4d01a58cf041929d6d24ff22a417c9e7d2874735
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2025-08-21 02:25:02 +0530

    refactor: Remove production-specific configurations and console logs from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production'
-console.log('isProd:', isProd);
-// const repo = '/new-pinddaan';
-const repo = '';
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: isProd ? repo : '',
-  assetPrefix: isProd ? `${repo}/` : '',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
